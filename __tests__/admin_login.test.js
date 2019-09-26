3dc1a7d6c86ccde27bea0e05f6d75788a91c6a5f
Alistair Laing <alistair.laing@digital.cabinet-office.gov.uk>
2019-09-26 12:09:37 +0100

    Setup jest-image-snapshot for visual regression

--- __tests__/admin_login.test.js
+++ __tests__/admin_login.test.js
@@ -1,3 +1,7 @@
+const { toMatchImageSnapshot } = require('jest-image-snapshot');
+
+expect.extend({ toMatchImageSnapshot });
+
 describe('Logging into Admin', () => {
   beforeAll(async () => {
     await page.goto('http://localhost')
@@ -10,14 +14,18 @@ describe('Logging into Admin', () => {
 
     test('user can login',  async () => {
       //Screenshot our home page
-      await page.screenshot({path: 'screenshot.png'})
-      //Find and click login
-      await page.click('a[href="/user/login"]')
-      await page.waitForSelector('.button-save')
+      const homePage = await page.screenshot({fullPage: true})
+      // Do a visual regression test
+      expect(homePage).toMatchImageSnapshot()
+
+      //Find login link and navigate to page
+      await page.click('a[href="/user/login"]',{waitUntil: 'domcontentloaded'})
+
       await expect(page.title()).resolves.toMatch('Digital Marketplace')
       // Screenshot our login page
-      await page.screenshot({path: 'screenshot1.png'})
-
+      const loginPage = await page.screenshot({fullPage: true})
+      // Do a visual regression test
+      expect(loginPage).toMatchImageSnapshot()
     })
   })
 })
